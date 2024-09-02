f98bbd6c8fc8f4beb527599cdb9fc3c892e11b9b
Syrocco <maire.raphael@yahoo.fr>
2024-09-02 12:49:55 +0200

    updated figures

--- Proof density/density energy.py
+++ Proof density/density energy.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.signal import savgol_filter
 
 def sortAccordingToA(A, *args):
     arg = np.argsort(A)
@@ -11,17 +11,28 @@ def sortAccordingToA(A, *args):
 
 
 def downsample(x, n=3, m=3):
-
+    
     x_padded = np.concatenate((x, x[:n - 1]))
 
     weights = np.ones(n)/n
     moving_avg = np.convolve(x_padded, weights, mode='valid')
     return moving_avg[:len(x)][::m]
 
+fontsizeMain = 23
+fontsize = 13
+plt.rcParams.update({
+    'font.size' : fontsizeMain,                   # Set font size to 11pt
+    'axes.labelsize': fontsizeMain,               # -> axis labels
+    'legend.fontsize': fontsizeMain,              # -> legends
+})    
+markersize = 10
+
 
 n = 3
 m = 2
         
+color = ["#02ecee","#fa03ee", "#00fe01","#fa8173","black","#ffd41c"]
+marker = ["^", "D","s", "o", "v", "<"]
 Lx = 23.690
 T = 1.367
 
@@ -53,36 +64,34 @@ Es[0] = (A["ES"]/T)[0]
 Eb[0] = (A["EB"]/T)[0]
 
 N = len(Es)
-color = [plt.get_cmap("cool")(i/(N-1/N)) for i in range(N)]
+#color = [plt.get_cmap("cool")(i/(N-1/N)) for i in range(N)]
 # Create figure and axes
-fig, axs = plt.subplots(2, 1, figsize=(6, 8), sharex=True, layout = "constrained")
+fig, axs = plt.subplots(2, 1, figsize=(6, 5), sharex=True, layout = "constrained")
 
 
 for i in range(N):
-    axs[0].plot(L[i][::m], downsample(density[i], n = n, m = m), color=color[i])
+    axs[0].plot(L[i][::m], downsample(density[i], n = n, m = m), color=color[i], marker = marker[i], markersize = markersize, mfc = "none", linestyle = "--")
 axs[0].set_ylabel('$\phi$')
+
 for i in range(N):
-    axs[1].plot(L[i][::m], downsample(q4[i], n = n, m = m), color=color[i])
-axs[1].set_ylabel('$q4$')
+    axs[1].plot(L[i][::m], downsample(q4[i], n = n, m = m), color=color[i], marker = marker[i], markersize = markersize, mfc = "none", linestyle = "--")
+axs[1].set_ylabel('$q_4$')
 axs[1].set_xlabel(r'$x/L_x$')
 plt.tight_layout()
 plt.subplots_adjust(hspace=0)
 fig.savefig("EDMD_density_q4_profiles.pdf")
 
-fig, axs = plt.subplots(2, 1, figsize=(6, 8), sharex=True, layout = "constrained")
+fig, axs = plt.subplots(2, 1, figsize=(6, 5), sharex=True, layout = "constrained")
 for i in range(N):
-    axs[0].plot(L[i][::m], downsample(Es[i], n = n, m = m), color=color[i])
+    axs[0].plot(L[i][::m], downsample(Es[i], n = n, m = m), color=color[i], marker = marker[i], markersize = markersize, mfc = "none", linestyle = "--")
 axs[0].set_ylabel('$E_s/T$')
 
 # Third subplot
 for i in range(N):
-    axs[1].plot(L[i][::m], downsample(Eb[i], n = n, m = m), color=color[i])
+    axs[1].plot(L[i][::m], downsample(Eb[i], n = n, m = m), color=color[i], marker = marker[i], markersize = markersize, mfc = "none", linestyle = "--")
 axs[1].set_xlabel(r'$x/L_x$')
 axs[1].set_ylabel('$E_b/T$')
 
-
-plt.tight_layout()
-plt.subplots_adjust(hspace=0)
 
 fig.savefig("EDMD_energy_profile.pdf")
 plt.show()
